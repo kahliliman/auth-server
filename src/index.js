69feec1ce8879d7b80215977839c0519d2f76c26
kahliliman <kahlil.iman@gmail.com>
2023-02-18 23:36:32 +0700

    add cors header

--- src/index.js
+++ src/index.js
@@ -52,6 +52,13 @@ const authenticateToken = (req, res, next) => {
   });
 };
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.post("/auth/register", (req, res) => {
   const user = new User(req.body);
 
